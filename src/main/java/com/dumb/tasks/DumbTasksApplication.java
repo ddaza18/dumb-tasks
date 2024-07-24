fd7d54935e0ce8fc712b0178defa788fb945e308
ddaza <ddaza681@gmail.com>
2024-07-24 03:14:02 -0500

    Initial Import

---
+++ src/main/java/com/dumb/tasks/DumbTasksApplication.java
@@ -0,0 +1,13 @@
+package com.dumb.tasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DumbTasksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DumbTasksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dumb/tasks/DumbTasksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dumb.tasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DumbTasksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
